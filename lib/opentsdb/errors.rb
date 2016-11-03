b4cb52874386fa3eea924f1b3995d83eb310c6f8
David Dieulivol <dadie@nine.ch>
2016-11-03 14:58:21 +0100

    :sparkles: Raise an exception if no tags or invalid tags are passed in.

--- lib/opentsdb/errors.rb
+++ lib/opentsdb/errors.rb
@@ -5,5 +5,11 @@ module OpenTSDB
         'Unable to connect or invalid connection data'
       end
     end
+
+    class InvalidTagsError < StandardError
+      def to_s
+        'The tags you provided are invalid. Note that you need at least one tag to proceed.'
+      end
+    end
   end
 end
